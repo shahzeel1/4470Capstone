bc201a02b69ca11a2a36ed4efc8e8532434b1468
shahzeel1 <35353418+shahzeel1@users.noreply.github.com>
2022-02-01 15:21:47 -0500

    trying to fix test issues

--- src/navigation/index.js
+++ src/navigation/index.js
@@ -6,7 +6,7 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import SignInScreen from "../screens/SignInScreen";
 /*import SignUpScreen from "../screens/SignUpScreen";
 import ConfirmEmailScreen from "../screens/ConfirmEmailScreen";*/
-import ForgotPasswordScreen from "../screens/Forgot Password Screen";
+import ForgotPasswordScreen from "../screens/ForgotPasswordScreen";
 /*import NewPasswordScreen from "../screens/NewPasswordScreen";
 import HomeScreen from "../screens/HomeScreen";*/
 
@@ -16,11 +16,9 @@ const Navigation = () => {
   return (
     <NavigationContainer>
       <Stack.Navigator screenOptions={{ headerShown: false }}>
-        {
-          <Stack.Screen name="SignIn" component={SignInScreen} />
-          /*<Stack.Screen name="SignUp" component={SignUpScreen} />
-        <Stack.Screen name="ConfirmEmail" component={ConfirmEmailScreen} />*/
-        }
+        <Stack.Screen name="SignIn" component={SignInScreen} />
+        {/*<Stack.Screen name="SignUp" component={SignUpScreen} />
+        <Stack.Screen name="ConfirmEmail" component={ConfirmEmailScreen} />*/}
         <Stack.Screen name="ForgotPassword" component={ForgotPasswordScreen} />
         {/*<Stack.Screen name="NewPassword" component={NewPasswordScreen} />
 
