cf3f7da60eddbfc2b21138f32645e0d62c1e3fd8
CameronMrailton <cameronrailton@hotmail.com>
2022-03-16 17:44:55 -0400

    Create Notifications.js
    basic notification page needs a way to display items and needs a new button component to make them look better and needs to get rid of bottom background bar

---
+++ src/screens/Notifications/Notifications.js
@@ -0,0 +1,167 @@
+import React, { useState } from "react";
+import {
+  View,
+  ScrollView,
+  Text,
+  StatusBar,
+  SafeAreaView,
+  Image,
+  StyleSheet,
+  useWindowDimensions,
+  TouchableWithoutFeedback,
+  Keyboard,
+  KeyboardAvoidingView,
+} from "react-native";
+
+import PersonImage from "../../../assets/images/user.png";
+import NotificationImage from "../../../assets/images/notification.png";
+import HomeImage from "../../../assets/images/city.png";
+import MessageImage from "../../../assets/images/email.png";
+import orgImage from "../../../assets/images/enterprise.png";
+
+import CustomButton from "../../components/CustomButton";
+import { useFonts, Prompt_500Medium } from "@expo-google-fonts/prompt";
+import AppLoading from "expo-app-loading";
+import { useNavigation } from "@react-navigation/native";
+
+
+const Notifications = () => {
+
+
+  const { height } = useWindowDimensions();
+
+
+  const navigation = useNavigation();
+
+
+  const onHomePressed = () => {
+    navigation.navigate("Home");
+  };
+  const onBackPressed = () => {
+    navigation.navigate("Home");
+  };
+  const onNotificationPressed = () => {
+  };
+  const onProfilePressed = () => {
+    navigation.navigate("ProfilePage");
+  };
+
+
+
+
+
+  let [fontsLoaded] = useFonts({
+    Prompt_500Medium,
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  }
+
+  return (
+      <KeyboardAvoidingView
+      behavior={Platform.OS === "ios" ? "padding" : "height"}
+      style={styles.container}
+      >
+
+        <View style={styles.headBar}>
+          <Text style={styles.headBarText}>
+                Notifications
+          </Text>
+          <CustomButton
+            text="Back"
+            onPress={onBackPressed}
+            type="SECONDARY"
+            style={styles.backButton}
+          />
+        </View>
+       <ScrollView Style={styles.scrollView}>
+            
+       </ScrollView>
+
+       <View style={styles.bottomBar}>
+        <CustomButton
+                icon={orgImage}
+                onPress={onHomePressed}
+                style={[styles.bottomBarIcon]}
+                type="TERTIARY"
+        />
+        <CustomButton
+                icon={PersonImage}
+                onPress={onProfilePressed}
+                style={[styles.bottomBarIcon]}
+                type="TERTIARY"
+        />
+        <CustomButton
+                icon={MessageImage}
+                onPress={onBackPressed}
+                style={[styles.bottomBarIcon]}
+                type="TERTIARY"
+        />
+        <CustomButton
+                icon={NotificationImage}
+                onPress={onNotificationPressed}
+                style={[styles.bottomBarIcon]}
+                type="TERTIARY"
+        />
+       </View>
+
+      </KeyboardAvoidingView>
+    );
+  }
+  
+  const styles = StyleSheet.create({
+    headBar:{
+      backgroundColor: "#2A3950",
+      height: 65,
+      borderBottomColor:"#111727",
+      borderBottomWidth:1,
+    },
+    headBarText:{
+      color: "#FFFFFF",
+      fontFamily: "Prompt_500Medium",
+      fontSize: 25,
+      textAlign: "center",
+      marginTop:8,
+    },
+    backButton:{
+      position: 'absolute',
+      left: 5,
+      width:72,
+      top: -2,
+      marginLeft:3,
+    },
+    scrollView: {
+      flex:1,
+      backgroundColor: "#2A3950",
+      marginHorizontal: 20,
+      alignSelf:"center",
+    },
+    container: {
+      flex: 1,
+      backgroundColor: "#2A3950",
+    },
+    bottomBar:{
+      backgroundColor: "black",
+      position: 'absolute',
+      bottom:0,
+      width:428,
+      height: 45,
+      borderTopColor:"#111727",
+      borderTopWidth:1,
+      flexDirection:'row',
+      
+    },
+    bottomBarIcon:{
+      maxWidth: 110,
+      maxHeight: 80,
+      height:60,
+      resizeMode:"contain",
+      flexGrow:1,
+      marginTop:4,
+    },
+
+
+  });
+  
+export default Notifications;
