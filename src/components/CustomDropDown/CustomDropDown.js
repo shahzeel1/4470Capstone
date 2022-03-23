2f98a20260a2bd4b42784a070a6cb6859479824a
CameronMrailton <cameronrailton@hotmail.com>
2022-03-23 16:50:08 -0400

    Update CustomDropDown.js
    made drop down  abstract

--- src/components/CustomDropDown/CustomDropDown.js
+++ src/components/CustomDropDown/CustomDropDown.js
@@ -1,36 +1,44 @@
 import { Prompt_500Medium } from "@expo-google-fonts/prompt";
-import React from "react";
-import { View, StyleSheet, Image, TextInput } from "react-native";
+import React, { useState } from "react";
+import { render } from "react-dom";
+import { View, StyleSheet, Image, TextInput,  Text, } from "react-native";
 import ModalSelector from "react-native-modal-selector";
 
-const CustomDropDown = ({ image, textInputValue }) => {
+
+
+
+const CustomDropDown = ({ 
+  image, 
+  textInputValue,
+  options = ["hello", "bye"],
+  placeholder,
+  answer,
+}) => {
   let index = 0;
+  const [selection, setSelection] = useState("");
   const data = [
-    { key: index++, section: true, label: "Fruits" },
-    { key: index++, label: "Red Apples" },
-    { key: index++, label: "Cherries" },
+    { key: index++, section: true, label: placeholder }
   ];
+    for(var i=0; i < options.length; i++){
+      data.push({key: i+1,  label: options[i] });
+     }
+
   return (
-    <View style={styles.container}>
-      <Image source={image} style={styles.icon} />
+    <View style={styles.createProjectContainer}>
+      <Image source={image} style={styles.iconCreateProject} />
       <ModalSelector
         data={data}
-        initValue="Select something yummy!"
         onChange={(option) => {
-          alert(`${option.label} (${option.key}) nom nom nom`);
+          setSelection( option.label )
         }}
         style={styles.input}
       >
         <TextInput
-          style={{
-            borderWidth: 1,
-            borderColor: "#2A3950",
-            padding: 10,
-            color: "#0F6BAC",
-          }}
-          editable={false}
-          placeholder="Select something yummy!"
-          value={textInputValue}
+        placeholder = {placeholder}
+        style={styles.textCreateProject}
+          value =  {selection}
+          placeholderTextColor={"#0F6BAC"}
+
         />
       </ModalSelector>
     </View>
@@ -38,29 +46,53 @@ const CustomDropDown = ({ image, textInputValue }) => {
 };
 
 const styles = StyleSheet.create({
-  container: {
-    width: "100%",
-    height: "14%",
+  createProjectContainer: {
     alignItems: "center",
     paddingHorizontal: 10,
     flexDirection: "row",
-    marginVertical: 5,
+    marginHorizontal:14,
+    marginVertical:8,
     borderColor: "#0F6BAC",
+    width:400,
+    height:60,
     borderWidth: 2,
     borderRadius: 25,
+    flex:1,
   },
   input: {
     width: "50%",
     fontFamily: "Prompt_500Medium",
     flex: 3,
   },
-  icon: {
+  iconCreateProject: {
     width: "50%",
     height: "50%",
     resizeMode: "contain",
     marginVertical: 10,
     flex: 1,
+    marginLeft:17,
   },
+  createPageText:{
+    color: "white",
+    height: "100%",
+    paddingLeft: 10,
+    alignItems: "center",
+    fontFamily: "Prompt_500Medium",
+    flex: 2,
+    fontSize:20,
+  },
+  textCreateProject:{
+    borderWidth: 1,
+    borderColor: "#2A3950",
+    padding: 10,
+    color: "white",
+    fontFamily: "Prompt_500Medium",
+    flex: 2,
+    fontSize:20,
+    marginLeft:16,
+  }
+
 });
 
 export default CustomDropDown;
+
