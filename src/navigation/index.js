e7b24231b0e1220ad728ada92f112e46fdb34be4
CameronMrailton <cameronrailton@hotmail.com>
2022-03-23 20:41:12 -0400

    Update index.js
    added myProjects page, project search page and messages page.

--- src/navigation/index.js
+++ src/navigation/index.js
@@ -10,14 +10,16 @@ import SignUpScreen from "../screens/SignUpScreen";
 import ClubOrCompany from "../screens/ClubOrCompanyScreen.js/ClubOrCompany";
 import ClubSignUpScreen from "../screens/ClubSignUpScreen";
 import CompanySignUpScreen from "../screens/CompanySignUpScreen";
-import CreateProject from "../screens/CreateProject";
-import ProfilePage from "../screens/ProfilePage";
-import Notifications from "../screens/Notifications";
-import ProjectPage from "../screens/ProjectPage";
 import IndividualSignUp from "../screens/IndividualSignUpScreen"
 import StudentSignUp from "../screens/IndividualSignUpScreen/StudentSignUpScreen/StudentSignUp";
 import FacultySignUp from "../screens/IndividualSignUpScreen/FacultySignUpScreen/FacultySignUp";
 import ProfessionalSignUp from "../screens/IndividualSignUpScreen/ProfessionalSignUpScreen/ProfessionalSignUp";
+import CreateProject from "../screens/CreateProject";
+import ProfilePage from "../screens/ProfilePage";
+import Notifications from "../screens/Notifications";
+import ProjectSearchPage from "../screens/ProjectSearchPage";
+import MessagePage from "../screens/MessagePage";
+import MyProjectsPage from "../screens/MyProjectsPage"
 
 const Stack = createNativeStackNavigator();
 
@@ -33,13 +35,15 @@ const Navigation = () => {
         <Stack.Screen name="ClubSignUp" component={ClubSignUpScreen} />
         <Stack.Screen name="CompanySignUp" component={CompanySignUpScreen} />
         <Stack.Screen name="ProfilePage" component={ProfilePage} />
-        <Stack.Screen name="ProjectPage" component={ProjectPage} />
+        <Stack.Screen name="ProjectSearchPage" component={ProjectSearchPage} />
         <Stack.Screen name="Notification" component={Notifications} />
         <Stack.Screen name="IndividualSignUp" component={IndividualSignUp} />
         <Stack.Screen name="CreateProject" component={CreateProject} />
         <Stack.Screen name="StudentSignUp" component={StudentSignUp} />
         <Stack.Screen name="FacultySignUp" component={FacultySignUp} />
         <Stack.Screen name="ProfessionalSignUp" component={ProfessionalSignUp} />
+        <Stack.Screen name="MessagePage" component={MessagePage} />
+        <Stack.Screen name="MyProjectsPage" component={MyProjectsPage} />
 
       </Stack.Navigator>
     </NavigationContainer>
