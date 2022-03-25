5d59f1bb77cd5c94d048c78198b81c2c904cc707
CameronMrailton <cameronrailton@hotmail.com>
2022-03-25 18:40:44 -0400

    Add files via upload
    adding display folders for visual items on certain pages

---
+++ src/components/ApplicantDisplay/ApplicantDisplay.js
@@ -0,0 +1,58 @@
+import { Prompt_600SemiBold } from "@expo-google-fonts/prompt";
+import React from "react";
+import { Text, StyleSheet, Pressable, Image, ImageBackground } from "react-native";
+
+const ApplicantDisplay = ({
+    onPress,
+    text,
+    icon,
+    type = "PRIMARY",
+    bgColor,
+    fgColor,
+    style,
+  }) => {
+
+    return (
+        <Pressable onPress={onPress} style={[NotificationStyles.container,style, ]} >
+            <Text style={[NotificationStyles.personWritting]}> Western </Text>
+            
+        </Pressable>
+    );
+
+  };
+
+const NotificationStyles = StyleSheet.create({
+        //profile badge, project , club item style
+        container: {
+          alignItems: "center",
+      
+          flexDirection: "column",
+          flexDirection: "row",
+          marginVertical: 5,
+          borderColor: "#0F6BAC",
+          borderWidth: 2,
+          width:"100%",
+          height:164,
+          borderRadius: 25,
+          borderTopWidth:45,
+        },
+        image: {
+          width:160,
+          height:140,
+          resizeMode: "contain",
+          marginVertical: 10,
+          flex: 1,
+        },
+        personWritting:{
+          left:0,
+          top:-80,
+          fontSize:28,
+        },
+        MessegeContent:{
+          fontSize:28,
+          color:"white",
+          left:-20,
+        },
+});
+
+export default ApplicantDisplay;
---
+++ src/components/ProfileClubDisplay/ProfileClubDisplay.js
@@ -0,0 +1,72 @@
+import { Prompt_600SemiBold } from "@expo-google-fonts/prompt";
+import React from "react";
+import { Text, StyleSheet, Pressable, Image, ImageBackground } from "react-native";
+
+const ProfileClubDisplay = ({
+    onPress,
+    text,
+    icon,
+    type = "PRIMARY",
+    bgColor,
+    fgColor,
+    style,
+  }) => {
+if(type=="profileItem") {
+  return (
+      <Pressable onPress={onPress} style={[ProfileStyles.container,style, ]} >
+        <ImageBackground source={icon} style={ProfileStyles.image}>
+          <Text style={[ProfileStyles.bottomWritting]}> Western </Text>
+          
+        </ImageBackground> 
+        
+      </Pressable>
+  );
+}
+  else if (type=="ProjectItem"){
+    return (
+      <Pressable onPress={onPress} style={[NotificationStyles.container,style, ]} >
+        <ImageBackground source={icon} style={NotificationStyles.image}>
+          <Text style={[NotificationStyles.bottomWritting]}> Western </Text>
+          
+        </ImageBackground> 
+        
+      </Pressable>
+  );
+  }
+};
+
+const ProfileStyles = StyleSheet.create({
+  //profile badge, project , club item style
+  container: {
+    width: "100%",
+    alignItems: "center",
+    height:200,
+    paddingHorizontal: 10,
+    flexDirection: "row",
+    marginVertical: 5,
+    borderColor: "#0F6BAC",
+    borderWidth: 2,
+    borderRadius: 25,
+    marginLeft:16,
+    flex:1,
+    borderBottomWidth:40,
+  },
+  image: {
+    width:160,
+    height:140,
+    resizeMode: "contain",
+    marginVertical: 10,
+    flex: 1,
+  },
+  bottomWritting:{
+    bottom:-152,
+    right:-24,
+    alignItems: "center",
+    fontSize:28,
+  },
+});
+//notification item style
+
+//project item style
+
+export default ProfileClubDisplay;
