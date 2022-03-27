59cf9ebedb8af75d3285677b61da104a4959294e
CameronMrailton <cameronrailton@hotmail.com>
2022-03-27 17:09:49 -0400

    Update MessageDisplay.js
    added message display.

--- src/components/MessageDisplay/MessageDisplay.js
+++ src/components/MessageDisplay/MessageDisplay.js
@@ -1,58 +1,108 @@
 import { Prompt_600SemiBold } from "@expo-google-fonts/prompt";
 import React from "react";
-import { Text, StyleSheet, Pressable, Image, ImageBackground } from "react-native";
+import { Text, StyleSheet, Pressable, Image, ImageBackground, View,  Dimensions } from "react-native";
+
+
+import orgImage from "../../../assets/images/book.png";
 
 const MessageDisplay = ({
     onPress,
     text,
-    icon,
+    icon = orgImage,
     type = "PRIMARY",
     bgColor,
     fgColor,
     style,
+    sender = "Facebook",
+    received = "16 days Ago",
+    notificationName = "Message",
+    summary = "This is a summary of the message testing the length"
   }) => {
 
-    return (
-        <Pressable onPress={onPress} style={[NotificationStyles.container,style, ]} >
-            <Text style={[NotificationStyles.personWritting]}> Western </Text>
-            
+    const Category = (props) => <Text style={{fontWeight: 'bold',  color: "white",
+    fontFamily: "Prompt_500Medium"}}>{props.children}</Text>
+
+      return (
+        <Pressable onPress={onPress} style={[StylesMessage.container]} >
+          <Image source={orgImage} style={StylesMessage.image} />
+            <View sytle={[StylesMessage.Textbox]}>
+              <Text numberOfLines={1}  style={[StylesMessage.senderText]}><Category>{sender}</Category></Text>
+
+              <Text numberOfLines={1}  style={[StylesMessage.MessageText ]}>{summary}</Text>
+              <Text numberOfLines={1} style={[StylesMessage.postedWriting]}>{received}</Text>
+              <View style={StylesMessage.separator} />
+            </View>
         </Pressable>
-    );
+      );
 
-  };
+};
 
-const NotificationStyles = StyleSheet.create({
-        //profile badge, project , club item style
-        container: {
-          alignItems: "center",
-      
-          flexDirection: "column",
-          flexDirection: "row",
-          marginVertical: 5,
-          borderColor: "#0F6BAC",
-          borderWidth: 2,
-          width:"100%",
-          height:164,
-          borderRadius: 25,
-          borderTopWidth:45,
-        },
-        image: {
-          width:160,
-          height:140,
-          resizeMode: "contain",
-          marginVertical: 10,
-          flex: 1,
-        },
-        personWritting:{
-          left:0,
-          top:-80,
-          fontSize:28,
-        },
-        MessegeContent:{
-          fontSize:28,
-          color:"white",
-          left:-20,
-        },
+const StylesMessage = StyleSheet.create({
+  container: {
+    flexDirection:'row',
+  },
+  Textbox:{
+      bottom:0,
+  },
+  image: {
+    width:55,
+    height:45,
+    resizeMode:'contain',
+    top:23,
+    left:8,
+  },
+  senderText:{
+    fontSize:20,
+    left:20,
+    top:16,
+    color: "#0F6BAC",
+    fontFamily: 'Roboto',
+    flexWrap:'wrap',
+  },
+  notificationTypeText:{
+    flexWrap:'wrap',
+    fontSize:16,
+    left:20,
+    top:16,
+    color: "#0F6BAC",
+    fontFamily: 'Roboto',
+
+  },
+  MessageText:{
+    fontSize:16,
+    left:20,
+    top:12,
+    color: "#0F6BAC",
+    fontFamily: 'Roboto',
+    flexWrap:'wrap',
+    maxWidth:300,
+  },
+  
+  postedWriting:{
+  fontSize:14,
+  color: "#0F6BAC",
+  textAlign:"right",
+  right:-20,
+  top:10,
+  bottom:-5,
+  
+  },
+
+  separator: {
+    color:"#483d8b",
+    borderColor :"#483d8b",
+    borderWidth: 1,
+    marginLeft:20,
+    marginRight:-25,
+    bottom:-10,
+    height:2,
+    borderRadius: 25,
+  },
+
+
 });
+//notification item style
+
+//project item style
 
 export default MessageDisplay;
